81fe29c06e4d088eab2bcdedceee120a27f30c8e
Aristotle Pagaltzis <pagaltzis@gmx.de>
2016-10-20 17:05:45 +0200

    base: only hide $INC[-1] . from optional loads

--- dist/base/t/lib/BaseIncExtender.pm
+++ dist/base/t/lib/BaseIncExtender.pm
@@ -1,7 +1,7 @@
 package BaseIncExtender;
 
 BEGIN { package main;
-    isnt $INC[-1], '.', 'no trailing dot in @INC during module load from base';
+    isnt $INC[-1], '.', 'no trailing dot in @INC during optional module load from base';
     is 0+(grep ref eq 'CODE', @INC), 2, '... but the expected dummy hook';
 }
 
